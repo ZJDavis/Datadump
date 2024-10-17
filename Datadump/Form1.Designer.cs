a92515625938910b219cbfba81604fa96042bb3b
Zack Davis <ryvenzd@gmail.com>
2024-10-17 08:25:39 -0700

    Initial build complete

--- Datadump/Form1.Designer.cs
+++ Datadump/Form1.Designer.cs
@@ -29,13 +29,93 @@ namespace Datadump
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnLoadCsv = new System.Windows.Forms.Button();
+            this.dateTimePickerStart = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePickerEnd = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnExportToExcel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnLoadCsv
+            // 
+            this.btnLoadCsv.Location = new System.Drawing.Point(20, 20);
+            this.btnLoadCsv.Name = "btnLoadCsv";
+            this.btnLoadCsv.Size = new System.Drawing.Size(100, 30);
+            this.btnLoadCsv.TabIndex = 0;
+            this.btnLoadCsv.Text = "Load CSV";
+            this.btnLoadCsv.UseVisualStyleBackColor = true;
+            this.btnLoadCsv.Click += new System.EventHandler(this.btnLoadCsv_Click_1);
+            // 
+            // dateTimePickerStart
+            // 
+            this.dateTimePickerStart.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePickerStart.Location = new System.Drawing.Point(150, 20);
+            this.dateTimePickerStart.Name = "dateTimePickerStart";
+            this.dateTimePickerStart.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePickerStart.TabIndex = 1;
+            // 
+            // dateTimePickerEnd
+            // 
+            this.dateTimePickerEnd.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePickerEnd.Location = new System.Drawing.Point(150, 60);
+            this.dateTimePickerEnd.Name = "dateTimePickerEnd";
+            this.dateTimePickerEnd.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePickerEnd.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(356, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Start Date";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(356, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "End Date";
+            // 
+            // btnExportToExcel
+            // 
+            this.btnExportToExcel.Location = new System.Drawing.Point(20, 60);
+            this.btnExportToExcel.Name = "btnExportToExcel";
+            this.btnExportToExcel.Size = new System.Drawing.Size(100, 30);
+            this.btnExportToExcel.TabIndex = 5;
+            this.btnExportToExcel.Text = "Create XLS";
+            this.btnExportToExcel.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(443, 123);
+            this.Controls.Add(this.btnExportToExcel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTimePickerEnd);
+            this.Controls.Add(this.dateTimePickerStart);
+            this.Controls.Add(this.btnLoadCsv);
+            this.Name = "Form1";
+            this.Text = "LoadCSV";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnLoadCsv;
+        private System.Windows.Forms.DateTimePicker dateTimePickerStart;
+        private System.Windows.Forms.DateTimePicker dateTimePickerEnd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnExportToExcel;
     }
 }
 
--- Datadump/Form1.cs
+++ Datadump/Form1.cs
@@ -1,20 +1,121 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
+using System.Data.SQLite;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using OfficeOpenXml;
 
 namespace Datadump
 {
     public partial class Form1 : Form
     {
+        private SQLiteConnection sqliteConn;
+
         public Form1()
         {
             InitializeComponent();
+            // Initialize SQLite connection.
+            sqliteConn = new SQLiteConnection("Data Source=sample.db;Version=3;");
+            sqliteConn.Open();
+            CreateTable();
+        }
+
+        private void CreateTable()
+        {
+            string createTableQuery = @"
+                CREATE TABLE IF NOT EXISTS Data (
+                    Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    Date TEXT,
+                    Column1 TEXT,
+                    Column2 TEXT
+                    -- Add other columns as needed based on your CSV structure.
+                )";
+            using (var cmd = new SQLiteCommand(createTableQuery, sqliteConn))
+            {
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        private void btnLoadCsv_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                ofd.Filter = "CSV files (*.csv)|*.csv";
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    LoadCsvIntoDatabase(ofd.FileName);
+                }
+            }
+        }
+
+        private void LoadCsvIntoDatabase(string filePath)
+        {
+            var lines = File.ReadAllLines(filePath);
+            var headers = lines[0].Split(',');
+
+            using (var transaction = sqliteConn.BeginTransaction())
+            {
+                foreach (var line in lines.Skip(1))
+                {
+                    var values = line.Split(',');
+
+                    string insertQuery = "INSERT INTO Data (Date, Column1, Column2) VALUES (@Date, @Column1, @Column2)";
+                    using (var cmd = new SQLiteCommand(insertQuery, sqliteConn))
+                    {
+                        cmd.Parameters.AddWithValue("@Date", values[0]);
+                        cmd.Parameters.AddWithValue("@Column1", values[1]);
+                        cmd.Parameters.AddWithValue("@Column2", values[2]);
+                        // Add other columns as needed.
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+                transaction.Commit();
+            }
+
+            MessageBox.Show("CSV file loaded successfully.");
+        }
+
+        private void btnExportToExcel_Click(object sender, EventArgs e)
+        {
+            DateTime startDate = dateTimePickerStart.Value;
+            DateTime endDate = dateTimePickerEnd.Value;
+            ExportDataToExcel(startDate, endDate);
+        }
+
+        private void ExportDataToExcel(DateTime startDate, DateTime endDate)
+        {
+            string selectQuery = "SELECT * FROM Data WHERE Date BETWEEN @startDate AND @endDate";
+            using (var cmd = new SQLiteCommand(selectQuery, sqliteConn))
+            {
+                cmd.Parameters.AddWithValue("@startDate", startDate.ToString("yyyy-MM-dd"));
+                cmd.Parameters.AddWithValue("@endDate", endDate.ToString("yyyy-MM-dd"));
+
+                using (SQLiteDataAdapter adapter = new SQLiteDataAdapter(cmd))
+                {
+                    DataTable dataTable = new DataTable();
+                    adapter.Fill(dataTable);
+
+                    using (SaveFileDialog sfd = new SaveFileDialog { Filter = "Excel files (*.xlsx)|*.xlsx" })
+                    {
+                        if (sfd.ShowDialog() == DialogResult.OK)
+                        {
+                            using (var package = new ExcelPackage())
+                            {
+                                var worksheet = package.Workbook.Worksheets.Add("Data");
+                                worksheet.Cells["A1"].LoadFromDataTable(dataTable, true);
+                                package.SaveAs(new FileInfo(sfd.FileName));
+                                MessageBox.Show("Data exported to Excel successfully.");
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void btnLoadCsv_Click_1(object sender, EventArgs e)
+        {
+
         }
     }
 }
